cffe1db68e3e7fbf5d68d76d4db10afe9419d291
Konstantin Dobrychev <konstantin.dobrychev@rubius.com>
2021-04-19 22:03:31 +0700

    fix #2

--- 02/src/main.py
+++ 02/src/main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     try:
         device: Device = open_device('/devices/dev3')
 
-        for i in range(5):
+        for i in range(3):
             print(read_line(device))
 
     except Exception as exception:
