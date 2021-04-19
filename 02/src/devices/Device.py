62617ac832c2a9d2db21ed74a71b2d1108cc7aed
Konstantin Dobrychev <konstantin.dobrychev@rubius.com>
2021-04-19 18:55:47 +0700

    Added files for the 2nd class.

---
+++ 02/src/devices/Device.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+from typing import List
+
+from devices.DeviceMode import DeviceMode
+
+
+def __take_line(collection: List[str]) -> str:
+    return collection.pop(0)
+
+
+@dataclass
+class Device:
+    mode: DeviceMode
+    data: List[str]
+
+
+def is_readable_device(device: Device) -> bool:
+    """Показывает, можно ли читать из устройства."""
+    return DeviceMode.ReadOnly in device.mode
+
+
+def is_writable_device(device: Device) -> bool:
+    """Показывает, можно писать в устройства."""
+    return DeviceMode.WriteOnly in device.mode
+
+
+def read_line(device: Device) -> str:
+    """
+    Читает строку текста из устройства
+    :param device: устройство
+    :return: считанная строка
+    :exception IndexError если строка не может быть прочитана из устройства
+    :exception PermissionError если устройство не открыто на чтение
+    """
+    if not is_readable_device(device):
+        raise PermissionError('Reading from the devices not allowed.')
+
+    return __take_line(device.data)
+
+
+def open_device(name: str) -> Device:
+    """
+    Открывает указанное устройство.
+    :param name: имя устройства
+    :return: открытое устройство
+    :exception KeyError если устройство не зарегистрировано в таблице
+    """
+    devices = {
+        '/devices/dev0': Device(DeviceMode.ReadOnly, ['line_1', 'line_2']),
+        '/devices/dev1': Device(DeviceMode.WriteOnly, ['']),
+        '/devices/dev2': Device(DeviceMode.ReadWrite, []),
+        '/devices/dev3': Device(DeviceMode.ReadWrite, ['1', '2', '**', 'hello', '']),
+        '/devices/dev4': Device(DeviceMode.ReadOnly, ['line_1', 'line_2']),
+    }
+
+    try:
+        return devices[name]
+    except KeyError:
+        raise IOError('Device no found')
---
+++ 02/src/main.py
@@ -0,0 +1,12 @@
+from devices.Device import *
+
+
+if __name__ == '__main__':
+    try:
+        device: Device = open_device('/devices/dev3')
+
+        for i in range(5):
+            print(read_line(device))
+
+    except Exception as exception:
+        print(f'Error: {exception}')
